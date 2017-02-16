4642073c7b7d67b5aecee13c222e509d7c56cbc6
Luke Marsden <me@lukemarsden.net>
2017-02-16 15:17:37 +0000

    Fixup client

--- client/query.go
+++ client/query.go
@@ -1,31 +1,30 @@
 package query
 
 import (
-	"fmt"
 	"log"
 	"net/http"
-
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"time"
 )
 
 func main() {
 	req := func() {
-		resp, err := http.Get("http://localhost/")
+		_, err := http.Get("http://localhost/")
 		if err != nil {
 			log.Fatal(err)
 		}
+		log.Print(".")
 	}
 	// make some requests with a spike
 	// aim to simulate 1, 2, 3, 13, 23, 33, 34, 35, 36
-	for i = 0; i < 3; i++ {
+	for i := 0; i < 3; i++ {
 		req()
 		time.Sleep(1 * time.Second)
 	}
-	for i = 0; i < 30; i++ {
+	for i := 0; i < 30; i++ {
 		req()
-		time.Sleep(0.1 * time.Second)
+		time.Sleep(100 * time.Millisecond)
 	}
-	for i = 0; i < 3; i++ {
+	for i := 0; i < 3; i++ {
 		req()
 		time.Sleep(1 * time.Second)
 	}
