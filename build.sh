4a827adf927e788343b70a654e8abd22a49f181c
Luke Marsden <me@lukemarsden.net>
2017-02-23 18:31:15 +0000

    Update tags

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -xe
 CGO_ENABLED=0 go build -o server server.go
-docker build -t lmarsden/wlw:v0.1 .
-docker push lmarsden/wlw:v0.1
+docker build -t lmarsden/wlw:faster .
+docker push lmarsden/wlw:faster
